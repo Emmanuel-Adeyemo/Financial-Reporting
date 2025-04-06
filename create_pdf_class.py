4a0d18d68a8b7cd2b969bce02c6f467c6f557157
Emmanuel Adeyemo <eadeyemo2@gmail.com>
2025-04-05 19:11:18 -0500

    Update create_pdf_class.py

--- create_pdf_class.py
+++ create_pdf_class.py
@@ -27,18 +27,18 @@ class PDFReport(FPDF):
         self.ln()
 
     def add_table(self, monthly_totals):
-        # Add column headers
+        # Add column heads
         self.set_font('Helvetica', 'B', 12)
         self.cell(40, 10, 'Month', 1, 0, 'C')
         self.cell(40, 10, 'Total Debit (Naira)', 1, 0, 'C')
         self.cell(50, 10, 'Total Credit (Naira)', 1, 0, 'C')
         self.cell(60, 10, 'Remaining Balance (Naira)', 1, 1, 'C')
 
-        # Add table rows
+        # ADd table rows
         self.set_font('Helvetica', '', 12)
         for month, totals in monthly_totals.items():
             month_format = datetime.strptime(month, '%Y-%b')
-            self.cell(40, 10, month_format.strftime('%B, %Y'), 1, 0, 'C') # I prefer Month, yEar
+            self.cell(40, 10, month_format.strftime('%B, %Y'), 1, 0, 'C') # Month, yEar format
             self.cell(40, 10, f"{totals['DEBIT']:,.2f}", 1, 0, 'C')
             self.cell(50, 10, f"{totals['CREDIT']:,.2f}", 1, 0, 'C')
             self.cell(60, 10, f"{totals['BALANCE']:,.2f}", 1, 1, 'C')
