09b4e55300291460712674c60dde7bfcb65b919b
Emmanuel Adeyemo <eadeyemo2@gmail.com>
2025-04-05 20:07:51 -0500

    Update get_account_name.py

--- get_account_name.py
+++ get_account_name.py
@@ -2,21 +2,18 @@ def extract_metadata(pdf):
     """Extract account name and statement duration from the PDF."""
     account_name = None
 
-    # Define a list of possible phrases indicating account name
     account_keywords = [ "Account Name", "Account Holder", "Cust. Name"]
 
-    # Loop through each page in the PDF
     for page in pdf.pages:
-        # Extract the text content of the page
         text = page.extract_text()
         
         if text:
-            # Look for account name
             lines = text.split("\n")
             for line in lines:
+                #print(line.lower())
                 for keyword in account_keywords:
                     if keyword.lower() in line.lower():
-                        # Extract everything after the keyword
+                        
                         account_name = line.replace(keyword, "").strip()
                         break  # Stop once account name is found             
                
