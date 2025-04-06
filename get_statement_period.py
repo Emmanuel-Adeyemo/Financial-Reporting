a450ada75d5186dc1fc9b65888dc1d42287c0b56
Emmanuel Adeyemo <eadeyemo2@gmail.com>
2025-04-05 20:06:20 -0500

    Update get_statement_period.py

--- get_statement_period.py
+++ get_statement_period.py
@@ -4,15 +4,15 @@ def extract_metadata_from_transactions(transactions):
     statement_duration = None
 
     if transactions:
-        # Sort transactions by the TXN_DATE key to ensure chronological order
+        
         try:
             sorted_transactions = transactions #sorted(transactions, key=lambda x: x.get("TXN_DATE"))
 
-            # Extract the first and last dates
-            start_date = sorted_transactions[1].get("TXN_DATE")  # First transaction date
-            end_date = sorted_transactions[-2].get("TXN_DATE")  # Last transaction date
+            # get first and last dates
+            start_date = sorted_transactions[1].get("TXN_DATE")  # First txn date
+            end_date = sorted_transactions[-2].get("TXN_DATE")  # Last txn date
 
-            # Format the statement duration
+            # format out
             if start_date and end_date:
                 statement_duration = f"{start_date} to {end_date}"
         except Exception as e:
